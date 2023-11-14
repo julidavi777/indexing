91683d598c4257828ebe1f884dbc449c56192d27
julidavi777 <93684468+julidavi777@users.noreply.github.com>
2023-11-14 10:05:37 -0500

    header is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Button from './components/button';
+import Header from './components/Header';
 function App() {
 
   return (
     <>
-    <Button value='badge' type='submit' />
+    <div className=''>
+     <Header /> 
+    </div>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from "./button";
+import logo from "../assets/logo.png";
+import loupe from "../assets/icons/loupe.png";
+import wallet from "../assets/icons/wallet.png";
+import pencil from "../assets/icons/pencil.png";
+const Header = () => {
+
+
+  return (
+    <>
+    
+      <div className=' px-3 py-2 flex  justify-between items-center '>
+        <div className="w-48" id="logoSlot">
+          <img src={logo} alt='Logo de Indexart' className="" id="logoImage"  />
+        </div>
+         <div>
+          <Button value='badge' type='submit' />
+        </div>
+        <div className="text-white w-1/4 p-2 flex justify-start rounded-sm bg-neutral-600" >
+            <div className="pe-1 " ><img src={loupe} width="25" alt="Search" style={{ filter: 'invert(1)' }} /></div>
+            <input className="bg-neutral-600 placeholder:text-white w-full" type="search" placeholder="Search Ordinals" name="SearchOrdinals" id="SearchOrdinals"/>
+        </div>
+        <div>
+          <Button value='Inscribe your Ordinals' type='submit' color="secondary" icon={pencil} />
+        </div>
+        <div>
+          <Button value='Connect your Wallet' type='submit' icon={wallet}/>
+        </div> 
+      </div>
+      <hr className="mt-1 text-red-500"/>
+    </>
+  );
+};
+
+export default Header;
