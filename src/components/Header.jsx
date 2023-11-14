b4c7c5b46a4a8a4c5e6cd59c4ce9e300a2b98ca5
julidavi777 <93684468+julidavi777@users.noreply.github.com>
2023-11-14 10:18:53 -0500

    search bar modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,9 +17,9 @@ const Header = () => {
          <div>
           <Button value='badge' type='submit' />
         </div>
-        <div className="text-white w-1/4 p-2 flex justify-start rounded-sm bg-neutral-600" >
-            <div className="pe-1 " ><img src={loupe} width="25" alt="Search" style={{ filter: 'invert(1)' }} /></div>
-            <input className="bg-neutral-600 placeholder:text-white w-full" type="search" placeholder="Search Ordinals" name="SearchOrdinals" id="SearchOrdinals"/>
+        <div className="flex justify-center text-white w-1/4 p-2  rounded-sm bg-neutral-600" >
+            <div className="pe-1 z-0 " ><img src={loupe} width="25" alt="Search" style={{ filter: 'invert(1)' }} /></div>
+            <input className="z-10 bg-neutral-600 w-full h-full outline-none placeholder:text-white w-full" type="search" placeholder="Search Ordinals" name="SearchOrdinals" id="SearchOrdinals"/>
         </div>
         <div>
           <Button value='Inscribe your Ordinals' type='submit' color="secondary" icon={pencil} />
